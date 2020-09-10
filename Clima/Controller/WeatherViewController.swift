6c4be1a4d4799a78d31fad3504c240112d40d78a
macintoshhd <1234>
2020-09-10 16:03:59 +0700

    first commit

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,18 @@
+
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
